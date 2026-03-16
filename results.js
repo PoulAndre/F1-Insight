67c1e79d22160c0b53d697f5088466fcacf3f7c4
PoulAndre <poul.a@hotmail.no>
2026-03-16 23:47:03 +0100

    Fikset sortering av drivers i Results

--- results.js
+++ results.js
@@ -15,33 +15,37 @@ async function updateDriverInfo() {
         console.log(driverdata[0]);
         console.log(championsdata[0]);
 
+
+        driverdata.sort((a, b) => {
+            const driverOne = championsdata.find(c => c.driver_number === a.driver_number);
+            const driverTwo = championsdata.find(c => c.driver_number === b.driver_number);
+            return driverTwo.points_current - driverOne.points_current;
+        });
+
+
         const liste = document.getElementById("Drivers");
         driverdata.forEach((driver, index) => {
                 const champion = championsdata.find(c => c.driver_number === driver.driver_number);
-                    
-                    let poeng;
-                        if(champion) {
-                            poeng = champion.points_current;
-                        } else {
-                            poeng = "NADA";
-                        }
+               
+                let poeng;
+                if(champion) {
+                    poeng = champion.points_current;
+                } else {
+                    poeng = "NADA";
+                }
 
-        const one = "st";
-        const two = "nd";
-        const third = "rd";
-        const rest = "th";
-
-        const posisjon = index + 1;
-        let pos;
-        if(posisjon === 1) {
-            pos = one;
-        } else if(posisjon === 2) {
-            pos = two; 
-        } else if(posisjon === 3) {
-            pos = third;
-        } else {
-            pos = rest;
-        }
+                 
+                const posisjon = index + 1;
+                let pos;
+                if(posisjon === 1) {
+                    pos = "st";
+                } else if(posisjon === 2) {
+                    pos = "nd"; 
+                } else if(posisjon === 3) {
+                    pos = "rd";
+                } else {
+                    pos = "th";
+                }
         
 
             const li = document.createElement("li");
@@ -50,11 +54,11 @@ async function updateDriverInfo() {
                 <div class="Plass">
                     <p class="pos">${posisjon + pos}</p>
                     <p class="Driver">${driver.full_name}</p>
-                    <p class="Team">${poeng}</p>
+                    <p class="Team">${poeng }</p>
                 </div>
             `;
             liste.appendChild(li);
-            });
+        });
     } catch (error) {
          console.error("Klarte ikke hente data fra API-en din:", error);
     }
